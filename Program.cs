2542d6b1878229b10ffa14b6fea9f92bc7016c59
Ron Enubs <ronenubs@gmail.com>
2020-09-14 21:51:37 +0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SinglyLinkedListExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SList<string> strList = new SList<string>();
+            strList.AddFirst("MSP");//MSP
+            strList.AddFirst("newest");//newest MSP
+            strList.AddLast("BOS");//newest MSP BOS
+            
+            try
+            {
+                string removed = strList.RemoveFirst();//MSP BOS
+                Console.WriteLine("Remove: {0}", removed);
+                Console.WriteLine(strList.First());
+                string removed1 = strList.RemoveFirst();//BOS
+                Console.WriteLine("Remove: {0}", removed1);
+                Console.WriteLine(strList.First());
+                string removed2 = strList.RemoveFirst();//BOS
+                Console.WriteLine("Remove: {0}", removed2);
+                Console.WriteLine(strList.First());
+            }
+            catch(EmptyListException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ SNode.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SinglyLinkedListExample
+{
+    public class SNode<E>
+    {
+        private E element;
+        private SNode<E> next;
+
+        public SNode(E element, SNode<E> next)
+        {
+            this.Element = element;
+            this.Next = next;
+        }
+
+        public E Element { get => element; set => element = value; }
+        public SNode<E> Next { get => next; set => next = value; }
+    }
+}
